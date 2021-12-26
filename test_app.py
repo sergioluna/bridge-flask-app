a136550f419ca6181c3a55ae12bac205384f61eb
sergioluna <sluna@sergioluna.dev>
2021-12-25 20:10:30 -0800

    Create Flask app

---
+++ test_app.py
@@ -0,0 +1,10 @@
+import unittest
+from app import hello
+
+class TestBridgeApp(unittest.TestCase):
+
+  def test_hello(self):
+    self.assertEqual(hello(), "Hello World!\n")
+
+if __name__ == '__main__':
+  unittest.main()
