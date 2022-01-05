6ce269b03d176d413b1dcdb5664910a91d48f5e5
sergioluna <sluna@sergioluna.dev>
2022-01-04 18:03:50 -0800

    Add sanity check to api route

--- app.py
+++ app.py
@@ -5,5 +5,9 @@ app = Flask('bridge-app')
 def hello():
   return "Hello World!\n"
 
+@app.route('/api')
+def api_sanity_check():
+    return "API sanity check successful"
+
 if __name__ == '__main__':
   app.run(host = '0.0.0.0', port = 8080)
