69d0fdebffc4b8045e0de5008f8e632dff762d55
sergioluna <sluna@sergioluna.dev>
2022-01-08 14:52:54 -0800

    Trigger build pipeline

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ def hello():
 
 @app.route('/api')
 def api_sanity_check():
-    return "API sanity check successful"
+    return "API sanity check successful!"
 
 if __name__ == '__main__':
   app.run(host = '0.0.0.0', port = 8080)
